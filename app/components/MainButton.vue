edd0f295e571892f40a90e20e060b41b3ea33324
Jay van der Meij <jayvandermeij@live.nl>
2026-05-23 18:47:33 +0200

    button

---
+++ app/components/MainButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :href="href">
+    <slot />
+  </a>
+</template>
+
+<script setup>
+defineProps({
+  href: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<style scoped>
+a {
+  display: inline-block;
+  color: var(--off-white-wit);
+  background-color: var(--donker-blauw);
+  padding: 24px 8px;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: translateY(-3px);
+  }
+}
+</style>
--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -2,6 +2,21 @@
     <section>
         <div class="container">
             <h1>Welcome texts</h1>
+            <p class="text-normal">
+                homepage
+            </p>
+            <MainButton href="/about">Learn more</MainButton>
         </div>
     </section>
 </template>
+
+<style scoped>
+section {
+    background-image: radial-gradient(ellipse farthest-side at center, rgba(199, 210, 254, 0.4), rgba(233, 213, 255, 0.4), #fafafc 90%);
+    height: 70vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+}
+</style>
